9f27f758d29f073352008a0bfb5b2fa450f44296
David <davidyu37@gmail.com>
2018-02-02 18:24:36 +0800

    demo v8

--- express.js
+++ express.js
@@ -1,11 +1,11 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var compression = require('compression');
+var app = express();
 
+// app.use(compression);
 app.use("/", express.static(__dirname + '/build'));
 
-// app.get('/', function (req, res) {
-  // res.send('Hello World')
-// })
+var port = process.env.PORT || 3000;
 
-console.log('Listening on http://0.0.0.0:3000');
-app.listen(3000, '0.0.0.0')
+console.log('Listening on http://0.0.0.0:' + port);
+app.listen(port, '0.0.0.0');
